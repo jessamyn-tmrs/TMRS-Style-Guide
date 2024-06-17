c11b0e9521e839a7881ffe1ed2e4d1a16dddbbe6
jessamyn mctwigan <jmctwigan@tmrs.com>
2024-06-17 16:04:45 -0500

    adding style and view components and basic variables

---
+++ src/Components/App.tsx
@@ -0,0 +1,16 @@
+import '../Styles/App.css';
+import Homepage from './Homepage';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+      <Navbar />
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
