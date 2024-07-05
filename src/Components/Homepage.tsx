af5c847817e3e175b3086ff2181b4527e5dce721
jessamyn mctwigan <jmctwigan@tmrs.com>
2024-07-05 10:38:25 -0500

    removing warnings to build deploy

--- src/Components/Homepage.tsx
+++ src/Components/Homepage.tsx
@@ -1,24 +1,24 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import Cards from './Cards';
-import Scrollbar from './Scrollbar';
+// import Scrollbar from './Scrollbar';
 
 type Props = {}
 
 const Homepage = (props: Props) => {
-    const [backgroundColor, setBackgroundColor] = useState("white");
+    // const [backgroundColor, setBackgroundColor] = useState("white");
 
-    const handleScroll = (event: any) => {
-        const { scrollTop, scrollHeight, clientHeight } = event.target;
-        const scrollRatio = scrollTop / (scrollHeight - clientHeight);
+    // const handleScroll = (event: any) => {
+    //     const { scrollTop, scrollHeight, clientHeight } = event.target;
+    //     const scrollRatio = scrollTop / (scrollHeight - clientHeight);
 
-        if (scrollRatio > 0.5) {
-            setBackgroundColor("lightblue");
-        } else {
-            setBackgroundColor("pink");
-        }
-    };
+    //     if (scrollRatio > 0.5) {
+    //         setBackgroundColor("lightblue");
+    //     } else {
+    //         setBackgroundColor("pink");
+    //     }
+    // };
 
     return (
         <div className='homepage'>
